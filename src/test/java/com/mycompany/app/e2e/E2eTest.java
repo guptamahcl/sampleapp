ef933edf1af7c6772b98447a19a14df4e224f5ff
Mayank Gupta <guptama@hcl.com>
2018-09-26 15:33:11 +0530

    added e2e smoke and test configurations

---
+++ src/test/java/com/mycompany/app/e2e/E2eTest.java
@@ -0,0 +1,38 @@
+package com.mycompany.app;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class E2eTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public E2eTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( smokeTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
